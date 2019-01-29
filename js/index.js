d8bdb4e26f38139b06db06067b3034bb001f0a51
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-01-29 14:38:13 -0800

    Navigation done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// Your code goes here
+///	* [ ] `mouseover`
+//  * [ ] `keydown`
+//	* [ ] `wheel`
+//	* [ ] `drag / drop`
+//  * [ ] `load`
+//	* [ ] `focus`
+//	* [ ] `resize`
+//	* [ ] `scroll`
+//	* [ ] `select`
+//	* [ ] `dblclick`
+
+const nav = document.querySelectorAll(".nav-link");
+
+nav.forEach((currentvalue) => {
+    currentvalue.addEventListener("mouseover", (e) => {
+        currentvalue.style.color = "blue";
+        setTimeout(function() {
+            e.target.style.color = "";
+        }, 300);
+    });
+});
+
+//Sets the nav anchors from refreshing the page
+nav.forEach((currentvalue) => {
+    currentvalue.addEventListener("click", (e) => {
+        e.preventDefault();
+        TweenMax.to(e.target, 1, {x: 100, y: -25, rotation:1000, yoyo: true, repeat: 1});
+    });
+});
