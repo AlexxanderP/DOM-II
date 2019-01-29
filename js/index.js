c455e79aefae7da0abb8c609f86add89b271bff4
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-01-29 15:45:53 -0800

    9 completed

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 ///	* [X] `mouseover`
 //  * [X] `keydown`
-//	* [ ] `wheel`
+//	* [X] `wheel`
 //	* [ ] `drag / drop`
-//  * [ ] `load`
+//  * [X] `load`
 //	* [ ] `focus`
 //	* [ ] `resize`
-//	* [ ] `scroll`
+//	* [X] `scroll`
 //	* [ ] `select`
-//	* [ ] `dblclick`
+//	* [X] `dblclick`
 
 const nav = document.querySelectorAll(".nav-link");
 
+// Mouse Over
 nav.forEach((currentvalue) => {
     currentvalue.addEventListener("mouseover", (e) => {
         currentvalue.style.color = "blue";
@@ -28,14 +29,58 @@ nav.forEach((currentvalue) => {
     });
 });
 
-// keydown --- unique event
+// keydown 
 let pageTitle = document.querySelector('.logo-heading');
 document.addEventListener('keydown', function (event) {
-  let keyName = event.key
-  pageTitle.textContent = `key pressed: ${keyName}`
+    let keyName = event.key
+    pageTitle.textContent = `key pressed: ${keyName}`
 })
 
-// load --- unique event
+// load 
 window.addEventListener('load', function (event) {
     alert("Welcome to the Fun Bus! Woho!")
 })
+
+// wheel
+const welcomeTo = document.querySelector('h2');
+document.addEventListener('wheel', function (event) {
+  let deltaY = event.deltaY * 10
+    welcomeTo.textContent = `Scroll Away`
+})
+
+//Double click Lets Go!
+
+const letsGo = document.querySelector(".content-section .text-content h2");
+
+letsGo.addEventListener("dblclick", (e) => {
+    e.target.style.cssText = "font-size : 10rem";
+}); 
+
+
+// Double click Fun in The Sun
+const funSun = document.querySelector(".destination h4");
+
+funSun.addEventListener("dblclick", (e) => {
+    e.target.style.cssText = "font-size : 5rem";
+}); 
+
+
+
+
+
+
+
+// Copy for Intro Paragraph
+
+const introHeading = document.querySelector(".intro p")
+introHeading.addEventListener("copy", function(e) {
+    introHeading.innerText = "Don't copy me!"
+})
+
+
+//Double Click Pick Your Destination
+const destination = document.querySelector(".content-destination  h2");
+
+destination.addEventListener("dblclick", (e) => {
+    e.target.style.cssText = "font-size : 8rem";
+}); 
